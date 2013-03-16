9d47c82c5adf48b1eab213f8dafa8401e1f83830
flameo <flameo@gmx.net>
2013-03-16 21:41:14 +0100

    Update _subscribe.php
    refs #2624

--- templates/_subscribe.php
+++ templates/_subscribe.php
@@ -1,6 +1,6 @@
 <form id="feed_reader_subscription_form" method="post" action="<?= PluginEngine::getLink($plugin, array(), 'insert') ?>" style="margin: 0;">
-  <label>Newsfeed abbonieren:
+  <label>Newsfeed abonieren:
   <input type="text" name="url" />
   </label>
-  <input type="submit" value="<?= _("Hinzufügen")?>" />
+  <input type="submit" value="<?= _("Hinzufï¿½gen")?>" />
 </form>
